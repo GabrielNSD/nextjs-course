5c9c03a0ed47c87b3b335d42da4264e393923663
gabrielnsd <gabrielnsdantas@gmail.com>
2021-08-11 10:36:28 -0300

    S3M63

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,9 +4,17 @@ function ClientProjects() {
   const router = useRouter();
 
   console.log(router.query);
+
+  function loadProjectHandler() {
+    router.push({
+      pathname: "clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "project-a" },
+    });
+  }
   return (
     <div>
       <h1>Projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
