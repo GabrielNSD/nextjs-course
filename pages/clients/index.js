5cb0c5a1ba84500d08720dd98bee356e67df913b
gabrielnsd <gabrielnsdantas@gmail.com>
2021-08-11 10:28:54 -0300

    S3M61

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    { id: "gab", name: "Gabriel" },
+    { id: "man", name: "Manoel" },
+  ];
   return (
     <div>
       <h1>The clients page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
